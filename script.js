c9ba0a58a2f3674419b22714b97bd1d4986edf55
Sarthak Srivastava <sarthaksrivastavadtu712@gmail.com>
2020-05-25 18:40:13 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,451 @@
+var colors = ['#5fdde5', '#f4ea8e', '#ffcbcb', '#ffd8a6', '#ffd31d', 
+'#ffe0ac', '#ffacb7', '#ffc38b', '#f6d743', '#40bad5', '#f6d198', '#43d8c9', 
+'#ffa372', '#c3edea', '#ff6363',
+ '#f3c623', '#a3f7bf', '#dc2ade', '#a3f7bf', '#05dfd7', '#f35588', '#baf1a1', 
+ '#fc9d9d', '#feb72b',  '#deddfa', '#dab8f3', '#ea7ad7', '#be8abf', '#f0134d'];
+//var random_color = colors[Math.floor(Math.random() * colors.length)];
+//var cards=document.querrySeletorAll('card')
+
+
+
+$('#card_1').hover(function() {
+    var color = colors[Math.floor(Math.random() * colors.length)];
+    var colorString = color;
+    $('#card_1').css({"background-color":color});
+    
+});
+
+
+$('#card_2').hover(function() {
+    var color = colors[Math.floor(Math.random() * colors.length)];
+    var colorString = color;
+    $('#card_2').css({"background-color":color});
+    
+});
+
+$('#card_3').hover(function() {
+    var color = colors[Math.floor(Math.random() * colors.length)];
+    var colorString = color;
+    $('#card_3').css({"background-color":color});
+    
+});
+
+$('#card_4').hover(function() {
+    var color = colors[Math.floor(Math.random() * colors.length)];
+    var colorString = color;
+    $('#card_4').css({"background-color":color});
+    
+});
+
+$('#card_5').hover(function() {
+    var color = colors[Math.floor(Math.random() * colors.length)];
+    var colorString = color;
+    $('#card_5').css({"background-color":color});
+    
+});
+
+$('#card_6').hover(function() {
+    var color = colors[Math.floor(Math.random() * colors.length)];
+    var colorString = color;
+    $('#card_6').css({"background-color":color});
+    
+});
+
+$('#card_7').hover(function() {
+    var color = colors[Math.floor(Math.random() * colors.length)];
+    var colorString = color;
+    $('#card_7').css({"background-color":color});
+    
+});
+
+$('#card_8').hover(function() {
+    var color = colors[Math.floor(Math.random() * colors.length)];
+    var colorString = color;
+    $('#card_8').css({"background-color":color});
+    
+});
+
+$('#card_9').hover(function() {
+    var color = colors[Math.floor(Math.random() * colors.length)];
+    var colorString = color;
+    $('#card_9').css({"background-color":color});
+    
+});
+
+$('#card_10').hover(function() {
+    var color = colors[Math.floor(Math.random() * colors.length)];
+    var colorString = color;
+    $('#card_10').css({"background-color":color});
+    
+});
+
+$('#card_11').hover(function() {
+    var color = colors[Math.floor(Math.random() * colors.length)];
+    var colorString = color;
+    $('#card_11').css({"background-color":color});
+    
+});
+
+$('#card_12').hover(function() {
+    var color = colors[Math.floor(Math.random() * colors.length)];
+    var colorString = color;
+    $('#card_12').css({"background-color":color});
+    
+});
+
+$('#card_13').hover(function() {
+    var color = colors[Math.floor(Math.random() * colors.length)];
+    var colorString = color;
+    $('#card_13').css({"background-color":color});
+    
+});
+
+$('#card_14').hover(function() {
+    var color = colors[Math.floor(Math.random() * colors.length)];
+    var colorString = color;
+    $('#card_14').css({"background-color":color});
+    
+});
+
+$('#card_15').hover(function() {
+    var color = colors[Math.floor(Math.random() * colors.length)];
+    var colorString = color;
+    $('#card_15').css({"background-color":color});
+    
+});
+
+$('#card_16').hover(function() {
+    var color = colors[Math.floor(Math.random() * colors.length)];
+    var colorString = color;
+    $('#card_16').css({"background-color":color});
+    
+});
+
+$('#card_').hover(function() {
+    var color = colors[Math.floor(Math.random() * colors.length)];
+    var colorString = color;
+    $('#card_').css({"background-color":color});
+    
+});
+
+$('#card_17').hover(function() {
+    var color = colors[Math.floor(Math.random() * colors.length)];
+    var colorString = color;
+    $('#card_17').css({"background-color":color});
+    
+});
+
+$('#card_18').hover(function() {
+    var color = colors[Math.floor(Math.random() * colors.length)];
+    var colorString = color;
+    $('#card_18').css({"background-color":color});
+    
+});
+
+$('#card_19').hover(function() {
+    var color = colors[Math.floor(Math.random() * colors.length)];
+    var colorString = color;
+    $('#card_19').css({"background-color":color});
+    
+});
+
+$('#card_20').hover(function() {
+    var color = colors[Math.floor(Math.random() * colors.length)];
+    var colorString = color;
+    $('#card_20').css({"background-color":color});
+    
+});
+
+$('#card_21').hover(function() {
+    var color = colors[Math.floor(Math.random() * colors.length)];
+    var colorString = color;
+    $('#card_21').css({"background-color":color});
+    
+});
+
+$('#card_22').hover(function() {
+    var color = colors[Math.floor(Math.random() * colors.length)];
+    var colorString = color;
+    $('#card_22').css({"background-color":color});
+    
+});
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+$('#card_').hover(function() {
+    var color = colors[Math.floor(Math.random() * colors.length)];
+    var colorString = color;
+    $('#card_').css({"background-color":color});
+    
+});
+
+$('#card_').hover(function() {
+    var color = colors[Math.floor(Math.random() * colors.length)];
+    var colorString = color;
+    $('#card_').css({"background-color":color});
+    
+});
+
+$('#card_').hover(function() {
+    var color = colors[Math.floor(Math.random() * colors.length)];
+    var colorString = color;
+    $('#card_').css({"background-color":color});
+    
+});
+
+$('#card_').hover(function() {
+    var color = colors[Math.floor(Math.random() * colors.length)];
+    var colorString = color;
+    $('#card_').css({"background-color":color});
+    
+});
+
+$('#card_').hover(function() {
+    var color = colors[Math.floor(Math.random() * colors.length)];
+    var colorString = color;
+    $('#card_').css({"background-color":color});
+    
+});
+
+$('#card_').hover(function() {
+    var color = colors[Math.floor(Math.random() * colors.length)];
+    var colorString = color;
+    $('#card_').css({"background-color":color});
+    
+});
+
+$('#card_').hover(function() {
+    var color = colors[Math.floor(Math.random() * colors.length)];
+    var colorString = color;
+    $('#card_').css({"background-color":color});
+    
+});
+
+$('#card_').hover(function() {
+    var color = colors[Math.floor(Math.random() * colors.length)];
+    var colorString = color;
+    $('#card_').css({"background-color":color});
+    
+});
+
+$('#card_').hover(function() {
+    var color = colors[Math.floor(Math.random() * colors.length)];
+    var colorString = color;
+    $('#card_').css({"background-color":color});
+    
+});
+
+$('#card_').hover(function() {
+    var color = colors[Math.floor(Math.random() * colors.length)];
+    var colorString = color;
+    $('#card_').css({"background-color":color});
+    
+});
+
+$('#card_').hover(function() {
+    var color = colors[Math.floor(Math.random() * colors.length)];
+    var colorString = color;
+    $('#card_').css({"background-color":color});
+    
+});
+
+$('#card_').hover(function() {
+    var color = colors[Math.floor(Math.random() * colors.length)];
+    var colorString = color;
+    $('#card_').css({"background-color":color});
+    
+});
+
+$('#card_').hover(function() {
+    var color = colors[Math.floor(Math.random() * colors.length)];
+    var colorString = color;
+    $('#card_').css({"background-color":color});
+    
+});
+
+$('#card_').hover(function() {
+    var color = colors[Math.floor(Math.random() * colors.length)];
+    var colorString = color;
+    $('#card_').css({"background-color":color});
+    
+});
+
+$('#card_').hover(function() {
+    var color = colors[Math.floor(Math.random() * colors.length)];
+    var colorString = color;
+    $('#card_').css({"background-color":color});
+    
+});
+
+$('#card_').hover(function() {
+    var color = colors[Math.floor(Math.random() * colors.length)];
+    var colorString = color;
+    $('#card_').css({"background-color":color});
+    
+});
+
+$('#card_').hover(function() {
+    var color = colors[Math.floor(Math.random() * colors.length)];
+    var colorString = color;
+    $('#card_').css({"background-color":color});
+    
+});
+
+$('#card_').hover(function() {
+    var color = colors[Math.floor(Math.random() * colors.length)];
+    var colorString = color;
+    $('#card_').css({"background-color":color});
+    
+});
+
+$('#card_').hover(function() {
+    var color = colors[Math.floor(Math.random() * colors.length)];
+    var colorString = color;
+    $('#card_').css({"background-color":color});
+    
+});
+
+$('#card_').hover(function() {
+    var color = colors[Math.floor(Math.random() * colors.length)];
+    var colorString = color;
+    $('#card_').css({"background-color":color});
+    
+});
+
+$('#card_').hover(function() {
+    var color = colors[Math.floor(Math.random() * colors.length)];
+    var colorString = color;
+    $('#card_').css({"background-color":color});
+    
+});
+
+$('#card_').hover(function() {
+    var color = colors[Math.floor(Math.random() * colors.length)];
+    var colorString = color;
+    $('#card_').css({"background-color":color});
+    
+});
+
+$('#card_').hover(function() {
+    var color = colors[Math.floor(Math.random() * colors.length)];
+    var colorString = color;
+    $('#card_').css({"background-color":color});
+    
+});
+
+$('#card_').hover(function() {
+    var color = colors[Math.floor(Math.random() * colors.length)];
+    var colorString = color;
+    $('#card_').css({"background-color":color});
+    
+});
+
+$('#card_').hover(function() {
+    var color = colors[Math.floor(Math.random() * colors.length)];
+    var colorString = color;
+    $('#card_').css({"background-color":color});
+    
+});
+
+$('#card_').hover(function() {
+    var color = colors[Math.floor(Math.random() * colors.length)];
+    var colorString = color;
+    $('#card_').css({"background-color":color});
+    
+});
+
+$('#card_').hover(function() {
+    var color = colors[Math.floor(Math.random() * colors.length)];
+    var colorString = color;
+    $('#card_').css({"background-color":color});
+    
+});
+
+$('#card_').hover(function() {
+    var color = colors[Math.floor(Math.random() * colors.length)];
+    var colorString = color;
+    $('#card_').css({"background-color":color});
+    
+});
+
+$('#card_').hover(function() {
+    var color = colors[Math.floor(Math.random() * colors.length)];
+    var colorString = color;
+    $('#card_').css({"background-color":color});
+    
+});
+
+$('#card_').hover(function() {
+    var color = colors[Math.floor(Math.random() * colors.length)];
+    var colorString = color;
+    $('#card_').css({"background-color":color});
+    
+});
+
+$('#card_').hover(function() {
+    var color = colors[Math.floor(Math.random() * colors.length)];
+    var colorString = color;
+    $('#card_').css({"background-color":color});
+    
+});
+
+$('#card_').hover(function() {
+    var color = colors[Math.floor(Math.random() * colors.length)];
+    var colorString = color;
+    $('#card_').css({"background-color":color});
+    
+});
+
+$('#card_').hover(function() {
+    var color = colors[Math.floor(Math.random() * colors.length)];
+    var colorString = color;
+    $('#card_').css({"background-color":color});
+    
+});
+
+$('#card_').hover(function() {
+    var color = colors[Math.floor(Math.random() * colors.length)];
+    var colorString = color;
+    $('#card_').css({"background-color":color});
+    
+});
+
+$('#card_').hover(function() {
+    var color = colors[Math.floor(Math.random() * colors.length)];
+    var colorString = color;
+    $('#card_').css({"background-color":color});
+    
+});
+
+$('#card_').hover(function() {
+    var color = colors[Math.floor(Math.random() * colors.length)];
+    var colorString = color;
+    $('#card_').css({"background-color":color});
+    
+});
+
+$('#card_').hover(function() {
+    var color = colors[Math.floor(Math.random() * colors.length)];
+    var colorString = color;
+    $('#card_').css({"background-color":color});
+    
+});
+
+$('#card_').hover(function() {
+    var color = colors[Math.floor(Math.random() * colors.length)];
+    var colorString = color;
+    $('#card_').css({"background-color":color});
+    
+});
+
+$('#card_').hover(function() {
+    var color = colors[Math.floor(Math.random() * colors.length)];
+    var colorString = color;
+    $('#card_').css({"background-color":color});
+    
+});
+
+$('#card_').hover(function() {
+    var color = colors[Math.floor(Math.random() * colors.length)];
+    var colorString = color;
+    $('#card_').css({"background-color":color});
+    
+});
